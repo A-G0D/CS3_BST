eb46c719b90e219adaa05fcabbd0eedae2a82261
A-G0D <143042805+A-G0D@users.noreply.github.com>
2025-01-17 18:46:27 +0000

    .

--- Lab 13.01/BinarySearchTree.java
+++ Lab 13.01/BinarySearchTree.java
@@ -31,10 +31,33 @@ public class BinarySearchTree {
     }
     public void add(BinaryNode x) {add(root, x);}
     public void add(Comparable x) {add(root, new BinaryNode(x));}
+    public int height(BinaryNode rt) {
+       if(rt == null) return -1;
+       return Math.max(height(rt.getRight())+1, height(rt.getLeft())+1);
+    }
+    public int width() {
+        Queue<BinaryNode> queue = new LinkedList<>();
+        queue.add(root);
+        int mxWidth = 0;
+        while (!queue.isEmpty()) {
+            int levelWidth = queue.size();
+            mxWidth = Math.max(mxWidth, levelWidth);
+            for (int i = 0; i < levelWidth; i++) {
+                BinaryNode current = queue.poll();
+                if (current.getLeft() != null) queue.add(current.getLeft());
+                if (current.getRight() != null) queue.add(current.getRight());
+            }
+        }
+    return mxWidth;
+    }
     public void swap(BinaryNode a, BinaryNode b) {
         Comparable tmp = a.getValue();
         a.setValue(b.getValue());
         b.setValue(tmp);
     }
-    
+    public BinaryNode successor(BinaryNode x) {
+        x = x.getRight();
+        while (x.getLeft() != null) x = x.getLeft();
+        return x;
+    }
 }
