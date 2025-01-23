36f9e411d26b5b26313aa4923c26c20b97f4a18c
A-G0D <143042805+A-G0D@users.noreply.github.com>
2025-01-23 15:33:16 +0000

    .

--- Lab 13.01/BinarySearchTree.java
+++ Lab 13.01/BinarySearchTree.java
@@ -35,6 +35,9 @@ public class BinarySearchTree {
        if(rt == null) return -1;
        return Math.max(height(rt.getRight())+1, height(rt.getLeft())+1);
     }
+    public int levels() {
+        return height(root) + 1;
+    }
     public int width() {
         Queue<BinaryNode> queue = new LinkedList<>();
         queue.add(root);
@@ -60,4 +63,77 @@ public class BinarySearchTree {
         while (x.getLeft() != null) x = x.getLeft();
         return x;
     }
+    public ArrayList<BinaryNode> preorder() {
+        ArrayList<BinaryNode> ret = new ArrayList<>();
+        ret.add(root);
+        if (root.getLeft()!=null) ret.addAll(preorderA(root.getLeft()));
+        if (root.getRight()!=null) ret.addAll(preorderA(root.getRight()));
+        return ret;
+    }
+    public ArrayList<BinaryNode> preorderA(BinaryNode x) {
+        ArrayList<BinaryNode> ret = new ArrayList<>();
+        ret.add(x);
+        if (x.getLeft()!=null) ret.addAll(preorderA(x.getLeft()));
+        if (x.getRight()!=null) ret.addAll(preorderA(x.getRight()));
+        return ret;
+    }
+    public ArrayList<BinaryNode> postorder() {
+        ArrayList<BinaryNode> ret = new ArrayList<>();
+        if (root.getLeft()!=null) ret.addAll(postorderA(root.getLeft()));
+        if (root.getRight()!=null) ret.addAll(postorderA(root.getRight()));
+        ret.add(root);
+        return ret;
+    }
+    public ArrayList<BinaryNode> postorderA(BinaryNode x) {
+        ArrayList<BinaryNode> ret = new ArrayList<>();
+        if (x.getLeft()!=null) ret.addAll(postorderA(x.getLeft()));
+        if (x.getRight()!=null) ret.addAll(postorderA(x.getRight()));
+        ret.add(x);
+        return ret;
+    }
+    public ArrayList<BinaryNode> inorder() {
+        ArrayList<BinaryNode> ret = new ArrayList<>();
+        if (root.getLeft()!=null) ret.addAll(inorderA(root.getLeft()));
+        ret.add(root);
+        if (root.getRight()!=null) ret.addAll(inorderA(root.getRight()));
+        return ret;
+    }
+    public ArrayList<BinaryNode> inorderA(BinaryNode x) {
+        ArrayList<BinaryNode> ret = new ArrayList<>();
+        if (x.getLeft()!=null) ret.addAll(inorderA(x.getLeft()));
+        ret.add(x);
+        if (x.getRight()!=null) ret.addAll(inorderA(x.getRight()));
+        return ret;
+    }
+    public ArrayList<BinaryNode> revorder() {
+        ArrayList<BinaryNode> ret = new ArrayList<>();
+        if (root.getRight()!=null) ret.addAll(revorderA(root.getRight()));
+        ret.add(root);
+        if (root.getLeft()!=null) ret.addAll(revorderA(root.getLeft()));
+        return ret;
+    }
+    public ArrayList<BinaryNode> revorderA(BinaryNode x) {
+        ArrayList<BinaryNode> ret = new ArrayList<>();
+        if (x.getRight()!=null) ret.addAll(revorderA(x.getRight()));
+        ret.add(x);
+        if (x.getLeft()!=null) ret.addAll(revorderA(x.getLeft()));
+        return ret;
+    }
+    public ArrayList<BinaryNode> levelorder() {
+        ArrayList<BinaryNode> ret = new ArrayList<>();
+        Queue<BinaryNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int levelWidth = queue.size();
+            for (int i = 0; i < levelWidth; i++) {
+                BinaryNode current = queue.poll();
+                ret.add(current);
+                if (current.getLeft() != null) queue.add(current.getLeft());
+                if (current.getRight() != null) queue.add(current.getRight());
+            }
+        }
+        return ret;
+    }
+
+
 }
